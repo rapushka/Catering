85a73a9932ced6cac64cd051067721c8fac5ef35
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-25 19:17:24 +0300

    Infrastructure classes

---
+++ src/CateringCore/CateringCore/DbWorking/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Catering.DbWorking
+{
+	public class ApplicationContext : DbContext
+	{
+		// public DbSet<Student> Students { get; set; } = null!;
+
+		public ApplicationContext()
+		{
+			// Table<Student>.Value = Students;
+		}
+
+		public DbSet<T> GetTable<T>()
+			where T : class
+			=> Table<T>.Value;
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+			=> optionsBuilder.UseSqlite("Data Source = Organizer.db");
+
+		private static class Table<T>
+			where T : class
+		{
+			public static DbSet<T> Value = null!;
+		}
+	}
+}
---
+++ src/CateringCore/CateringCore/DbWorking/DataBaseConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catering.DbWorking;
+
+public class DataBaseConnection
+{
+	private static DataBaseConnection? _instance;
+	private ApplicationContext? _currentContext;
+
+	private DataBaseConnection() { }
+
+	public static DataBaseConnection Instance => _instance ??= new DataBaseConnection();
+
+	public ApplicationContext CurrentContext => _currentContext ?? throw new NullReferenceException();
+
+	public ObservableCollection<T> Observe<T>()
+		where T : class
+		=> CurrentContext.GetTable<T>().Local.ToObservableCollection();
+
+	public void ResetAll() => OpenDataBase();
+
+	public void OpenDataBase()
+	{
+		_currentContext = new ApplicationContext();
+		_currentContext.Database.Migrate();
+		_currentContext.Database.EnsureCreated();
+
+		// _currentContext.Students.Load();
+	}
+}
