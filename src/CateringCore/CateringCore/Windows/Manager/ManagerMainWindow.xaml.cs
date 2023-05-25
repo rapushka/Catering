99294f81d6187502faf065518dfd9e139fb69af7
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-26 02:20:33 +0300

    show Manager's full name

--- src/CateringCore/CateringCore/Windows/Manager/ManagerMainWindow.xaml.cs
+++ src/CateringCore/CateringCore/Windows/Manager/ManagerMainWindow.xaml.cs
@@ -5,8 +5,18 @@ namespace CateringCore.Windows;
 
 public partial class ManagerMainWindow : Window
 {
+	private const string ManagerSuffix = ", Менеджер";
+
+	private readonly Manager _manager;
+
 	public ManagerMainWindow(Manager manager)
 	{
+		_manager = manager;
 		InitializeComponent();
+	}
+
+	private void ManagerMainWindow_OnLoaded(object sender, RoutedEventArgs e)
+	{
+		EmployeeFullnameTextBlock.Text = _manager.Fullname + ManagerSuffix;
 	}
 }
