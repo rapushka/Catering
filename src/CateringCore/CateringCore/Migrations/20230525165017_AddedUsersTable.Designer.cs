973091724ac3f1ab91ee2c6798e7ea4a73c02f9c
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-25 19:50:33 +0300

    AddedUsersTable migration

--- src/CateringCore/CateringCore/Migrations/20230525165017_AddedUsersTable.Designer.cs
+++ src/CateringCore/CateringCore/Migrations/20230525165017_AddedUsersTable.Designer.cs
@@ -2,6 +2,7 @@
 using Catering.DbWorking;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CateringCore.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230525165017_AddedUsersTable")]
+    partial class AddedUsersTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.5");
