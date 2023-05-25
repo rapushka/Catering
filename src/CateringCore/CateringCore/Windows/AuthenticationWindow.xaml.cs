aebf691cc06351b2a91b6fcdc446b24291f91e05
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-26 01:19:21 +0300

    +ManagerMainWindow

--- src/CateringCore/CateringCore/Windows/AuthenticationWindow.xaml.cs
+++ src/CateringCore/CateringCore/Windows/AuthenticationWindow.xaml.cs
@@ -1,31 +1,44 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows;
 using Catering.DbWorking;
 using CateringCore.Model;
+using CateringCore.Windows;
 using OrganizerCore.Tools;
 
-namespace CateringCore
+namespace CateringCore;
+
+public partial class AuthenticationWindow
 {
-	public partial class AuthenticationWindow
+	public AuthenticationWindow()
 	{
-		public AuthenticationWindow()
+		InitializeComponent();
+		DbWorker.Open();
+	}
+
+	private void LoginButton_Click(object sender, RoutedEventArgs e)
+	{
+		var login = LoginTextBox.Text;
+		var password = PasswordTextBox.Text;
+
+		var user = DbWorker.Users.FirstOrDefault((u) => u.Authorize(login, password));
+
+		if (user is null)
 		{
-			InitializeComponent();
-			DbWorker.Open();
+			MessageBoxUtils.ShowError("Неправильный лоґин или пароль");
+			return;
 		}
 
-		private void LoginButton_Click(object sender, RoutedEventArgs e)
+		var userWindow = user switch
 		{
-			var login = LoginTextBox.Text;
-			var password = PasswordTextBox.Text;
+			Manager manager => new ManagerMainWindow(manager),
+			Cook cook       => throw new NotImplementedException(),
+			Courier courier => throw new NotImplementedException(),
+			_               => throw new ArgumentException("Unknown role!"),
+		};
 
-			var user = DbWorker.Users.FirstOrDefault((u) => u.Authorize(login, password));
-
-			if (user is null)
-			{
-				MessageBoxUtils.ShowError("Неправильный лоґин или пароль");
-				return;
-			}
-		}
+		Hide();
+		userWindow.ShowDialog();
+		Close();
 	}
 }
---
+++ src/CateringCore/CateringCore/Windows/Manager/ManagerMainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using CateringCore.Model;
+
+namespace CateringCore.Windows;
+
+public partial class ManagerMainWindow : Window
+{
+	public ManagerMainWindow(Manager manager)
+	{
+		InitializeComponent();
+	}
+}
