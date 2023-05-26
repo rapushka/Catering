f38a4dea727ab15a71e953544e0bd982c92b1740
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-27 02:21:26 +0300

    datagrid setup

--- src/CateringCore/CateringCore/Windows/Manager/Pages/Foods/FoodsListPage.xaml.cs
+++ src/CateringCore/CateringCore/Windows/Manager/Pages/Foods/FoodsListPage.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using CateringCore.Model;
-using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using OrganizerCore.Tools.Extensions;
 
 namespace CateringCore.Windows.Pages.Foods;
