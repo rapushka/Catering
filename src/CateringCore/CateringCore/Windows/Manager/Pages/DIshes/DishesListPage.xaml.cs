a6d29d8e9a5cb26a8cf2337c94d616d9043c62ed
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-26 03:57:06 +0300

    +Dishes types button

--- src/CateringCore/CateringCore/Windows/Manager/Pages/DIshes/DishesListPage.xaml.cs
+++ src/CateringCore/CateringCore/Windows/Manager/Pages/DIshes/DishesListPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using Catering.DbWorking;
+using OrganizerCore.DbWorking;
 
 namespace CateringCore.Windows.Pages;
 
@@ -7,5 +9,10 @@ public partial class DishesListPage
 	public DishesListPage()
 	{
 		InitializeComponent();
+	}
+
+	private void DishesListPage_OnLoaded(object sender, RoutedEventArgs e)
+	{
+		DishesDataGrid.ItemsSource = DbWorker.Context.Dishes.Observe();
 	}
 }
