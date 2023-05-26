ba0fe26a06d575d1a6d41914ebed9f19a31d2984
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-27 00:50:47 +0300

    +FoodsListPage

---
+++ src/CateringCore/CateringCore/Windows/Manager/Pages/Foods/FoodsListPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CateringCore.Windows.Pages.Foods;
+
+public partial class FoodsListPage : Page
+{
+	public FoodsListPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OpenCategories(object sender, RoutedEventArgs e)
+	{
+		
+	}
+
+	private void OpenTypes(object sender, RoutedEventArgs e)
+	{
+		
+	}
+}
