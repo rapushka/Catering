b00fb5000f2561dabe8ef3d7b712b59af6df50ff
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-26 20:23:15 +0300

    DishesListPage : EditableListPage<Dish>

--- src/CateringCore/CateringCore/Windows/Manager/Pages/DIshes/DishesListPage.xaml.cs
+++ src/CateringCore/CateringCore/Windows/Manager/Pages/DIshes/DishesListPage.xaml.cs
@@ -1,15 +1,16 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
 using Catering.DbWorking;
+using CateringCore.Model;
+using CateringCore.Windows.Pages.Common;
 using OrganizerCore.DbWorking;
 
 namespace CateringCore.Windows.Pages;
 
-public partial class DishesListPage
+public partial class DishesListPage : EditableListPage<Dish>
 {
-	public DishesListPage()
-	{
-		InitializeComponent();
-	}
+	public DishesListPage() => InitializeComponent();
 
 	private void DishesListPage_OnLoaded(object sender, RoutedEventArgs e)
 	{
@@ -20,4 +21,15 @@ public partial class DishesListPage
 	{
 		NavigationService!.Navigate(new DishesTypesListPage());
 	}
+
+	public override    DataGrid               DataGrid         => DishesDataGrid;
+	protected override string                 NameOfItemType   => "посуду";
+	protected override IEnumerable<UIElement> EditItemElements { get; }
+	protected override void                   SetupTable()     { }
+
+	protected override bool Filter(Dish dish)
+		=> dish.Title.Contains(SearchTitleTextBox.Text)
+		   && dish.Type == (DishType)SearchTypeComboBox.SelectedItem;
+
+	protected override void UpdateItem(ref Dish selected) { }
 }
