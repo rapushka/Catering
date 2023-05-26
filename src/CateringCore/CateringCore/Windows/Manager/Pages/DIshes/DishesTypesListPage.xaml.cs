b6c3003257931a343f32ad7b251c9861471eb414
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-26 04:05:39 +0300

    Load dishes table

--- src/CateringCore/CateringCore/Windows/Manager/Pages/DIshes/DishesTypesListPage.xaml.cs
+++ src/CateringCore/CateringCore/Windows/Manager/Pages/DIshes/DishesTypesListPage.xaml.cs
@@ -1,9 +1,18 @@
-﻿namespace CateringCore.Windows.Pages;
+﻿using System.Windows;
+using Catering.DbWorking;
+using OrganizerCore.DbWorking;
+
+namespace CateringCore.Windows.Pages;
 
 public partial class DishesTypesListPage
 {
 	public DishesTypesListPage()
 	{
 		InitializeComponent();
+	}
+
+	private void DishesTypesListPage_OnLoaded(object sender, RoutedEventArgs e)
+	{
+		DishesTypesDataGrid.ItemsSource = DbWorker.Context.DishTypes.Observe();
 	}
 }
