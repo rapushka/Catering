7c5dfa5e1822032f5a900d73a9fa1de52000cba9
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-26 04:23:33 +0300

    common data grid setup

--- src/CateringCore/CateringCore/Tools/Extension/DataGridExtensions.cs
+++ src/CateringCore/CateringCore/Tools/Extension/DataGridExtensions.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Controls;
+using System.Windows.Data;
+using Catering.DbWorking;
+using OrganizerCore.DbWorking;
 
 namespace OrganizerCore.Tools.Extensions;
 
@@ -11,4 +15,27 @@ public static class DataGridExtensions
 		@this.ScrollIntoView(item ?? throw new ArgumentNullException(nameof(item)));
 		@this.BeginEdit();
 	}
+	
+	public static void SetColumns(this DataGrid @this, Dictionary<string, string> columns)
+	{
+		@this.Columns.Clear();
+
+		foreach (var (columnName, binding) in columns)
+		{
+			@this.AddTextColumn(columnName, binding);
+		}
+	}
+
+	public static void Setup<T>(this DataGrid @this, FilterEventHandler filter)
+		where T : class
+	{
+		var viewSource = new CollectionViewSource
+		{
+			Source = DbWorker.Context.GetTable<T>().Observe(),
+		};
+
+		viewSource.Filter += filter;
+		@this.ItemsSource = viewSource.View;
+	}
+
 }
--- src/CateringCore/CateringCore/Windows/Manager/Pages/DIshes/DishesTypesListPage.xaml.cs
+++ src/CateringCore/CateringCore/Windows/Manager/Pages/DIshes/DishesTypesListPage.xaml.cs
@@ -1,18 +1,31 @@
-﻿using System.Windows;
-using Catering.DbWorking;
-using OrganizerCore.DbWorking;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Data;
+using CateringCore.Model;
+using OrganizerCore.Tools.Extensions;
 
 namespace CateringCore.Windows.Pages;
 
 public partial class DishesTypesListPage
 {
-	public DishesTypesListPage()
-	{
-		InitializeComponent();
-	}
+	public DishesTypesListPage() => InitializeComponent();
 
 	private void DishesTypesListPage_OnLoaded(object sender, RoutedEventArgs e)
 	{
-		DishesTypesDataGrid.ItemsSource = DbWorker.Context.DishTypes.Observe();
+		DishesTypesDataGrid.Setup<DishType>(Filter);
+		SetupDishesTypes();
+	}
+
+	private void Filter(object sender, FilterEventArgs e) { }
+
+	private void SetupDishesTypes()
+	{
+		DishesTypesDataGrid.SetColumns
+		(
+			new Dictionary<string, string>
+			{
+				["Наименование"] = nameof(DishType.Title),
+			}
+		);
 	}
 }
