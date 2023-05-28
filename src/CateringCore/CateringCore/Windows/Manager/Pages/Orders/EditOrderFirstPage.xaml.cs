d4177eec1f534903cba2fef5910a30c679836d9e
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-29 00:08:30 +0300

    +EditOrderFirstPage

---
+++ src/CateringCore/CateringCore/Windows/Manager/Pages/Orders/EditOrderFirstPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CateringCore.Windows.Pages.Orders;
+
+public partial class EditOrderFirstPage : Page
+{
+	public EditOrderFirstPage()
+	{
+		InitializeComponent();
+	}
+
+	private void NextButton_Click(object sender, RoutedEventArgs e)
+	{
+		
+	}
+}
