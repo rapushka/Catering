4b632b169027bc1cdbe98ab65bca0d924bd218cb
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-30 17:37:55 +0300

    cook window functions

--- src/CateringCore/CateringCore/Windows/Cook/CookMainWindow.xaml.cs
+++ src/CateringCore/CateringCore/Windows/Cook/CookMainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using CateringCore.Model;
+using CateringCore.Windows.Pages;
+using OrganizerCore.Tools.Extensions;
 
 namespace CateringCore.Windows.Cook;
 
@@ -9,16 +12,63 @@ public partial class CookMainWindow
 
 	private readonly Model.Cook _cook;
 
+	private Order? SelectedOrder => OrdersDataGrid.SelectedItem as Order;
+
 	public CookMainWindow(Model.Cook cook)
 	{
 		_cook = cook;
 		InitializeComponent();
 	}
 
-	private void CookMainWindow_OnLoaded(object sender, RoutedEventArgs e)
-		=> EmployeeFullnameTextBlock.Text = _cook.Fullname + CookSuffix;
+	private void Page_Load(object sender, RoutedEventArgs e)
+	{
+		EmployeeFullnameTextBlock.Text = _cook.Fullname + CookSuffix;
 
-	private void UpdateFilters(object sender, TextChangedEventArgs e) { }
+		UpdateView();
+	}
 
-	private void UpdateFilters(object sender, SelectionChangedEventArgs e) { }
+	private bool Filter(Order order)
+		=> order.State == Order.StateName.Processed
+		   && SearchOrderIdTextBox.NumberEqualsTo(order.Id);
+
+	private bool Filter(FoodInOrder foodInOrder)
+		=> foodInOrder.Order == SelectedOrder
+		   && foodInOrder.State == FoodInOrder.StateName.NotReady;
+
+	private void UpdateFilters(object sender, TextChangedEventArgs e) => UpdateView();
+
+	private void MarkAsCooked(object sender, RoutedEventArgs e)
+	{
+		if (OrdersDataGrid.EnsureSelected<FoodInOrder>("Блюдо в заказе", out var foodInOrder))
+		{
+			foodInOrder.State = FoodInOrder.StateName.Ready;
+			UpdateView();
+		}
+	}
+
+	private void UpdateView()
+	{
+		OrdersDataGrid.Setup<Order>(Filter);
+		SetupOrderColumns();
+		FoodsInOrderDataGrid.Setup<FoodInOrder>(Filter);
+		SetupFoodColumns();
+	}
+
+	private void SetupOrderColumns()
+	{
+		OrdersDataGrid
+			.ClearColumns()
+			.AddTextColumn("Номер", nameof(Order.Id))
+			.AddTextColumn("ФИО заказчика", nameof(Order.Fullname))
+			;
+	}
+
+	private void SetupFoodColumns()
+	{
+		OrdersDataGrid
+			.ClearColumns()
+			.AddTextColumn("Название", nameof(FoodInOrder.Food))
+			.AddTextColumn("Количество", nameof(FoodInOrder.Amount))
+			;
+	}
 }
